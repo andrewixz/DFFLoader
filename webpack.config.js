7b4659b7f5adc05845a2de8ae6718d8ec01a5a69
andrewixz <andrewixz@gmail.com>
2018-06-22 10:57:20 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/Loader.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'DFFLoader.js'
+    },
+    target: 'web'
+};
